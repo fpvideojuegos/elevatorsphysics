f6bb03a3afe962be7ca225b7e77ba5d9cc78ac0c
Francisco José Romero Medina <1376131+fromero98@users.noreply.github.com>
2019-11-18 09:14:31 +0100

    Add AssetCollection and Asset interface

--- src/ts/Assets.ts
+++ src/ts/Assets.ts
@@ -1,7 +1,18 @@
-export const Images = {
-    background: {
-        key: 'background',
+export const Images: AssetCollection = {
+    testBackground: {
+        key: 'testBackground',
         location: 'assets/images/background.png'
+    }, mainBackground: {
+        key: 'mainBackground',
+        location: 'A'
     }
 };
 
+interface AssetCollection {
+    [key: string]: Asset
+}
+
+interface Asset {
+    key: string,
+    location: string
+}
